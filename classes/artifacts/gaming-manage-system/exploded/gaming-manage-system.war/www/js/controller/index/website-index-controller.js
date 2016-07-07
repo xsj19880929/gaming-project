dbe5b5b05b0b17878d2d0f11eb8705683b2f0bc4
soldier.xu <soldier.xu@suryani.cn>
2016-07-07 16:41:32 +0800

    删减项目

--- classes/artifacts/gaming-manage-system/exploded/gaming-manage-system.war/www/js/controller/index/website-index-controller.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author miller
- */
-(function (angular) {
-    var module = angular.module('app');
-    module.controller('WebsiteIndexController', ['$scope', '$http', '$location', '$state', function ($scope, $http, $location, $state) {
-    }]);
-})(angular);
--- classes/artifacts/gaming-manage-system/exploded/gaming-manage-system.war/www/js/controller/product-controller.js
+++
@@ -1,9 +0,0 @@
-(function (angular) {
-
-    var app = angular.module('app');
-
-    app.controller('ProductController', ['$scope', function ($scope) {
-        $scope.message = 'wechat-system';
-    }]);
-
-})(angular);
