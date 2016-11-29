f912f438ce3182e2c04082d447f55d51cef3868a
soldier.xu <soldier.xu@suryani.cn>
2016-11-29 14:48:20 +0800

    评论内容加上

--- gaming-website/src/main/java/com/ygccw/website/interceptor/AccessInterceptor.java
+++ gaming-website/src/main/java/com/ygccw/website/interceptor/AccessInterceptor.java
@@ -1,15 +1,19 @@
 package com.ygccw.website.interceptor;
 
+import core.framework.web.site.SiteSettings;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AccessInterceptor extends HandlerInterceptorAdapter {
+    @Inject
+    SiteSettings siteSettings;
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        request.setAttribute("currentUrl", request.getRequestURI());
+        request.setAttribute("currentUrl", siteSettings.baseUrl() + request.getRequestURI());
     }
 }
