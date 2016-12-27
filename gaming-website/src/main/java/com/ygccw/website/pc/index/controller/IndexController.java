505d09f9ca3f84cfbd5e231b3297d6b29a8a1ed8
soldier.xu <soldier.xu@suryani.cn>
2016-12-27 17:32:39 +0800

    搜索页面待完成

--- gaming-website/src/main/java/com/ygccw/website/pc/index/controller/IndexController.java
+++ gaming-website/src/main/java/com/ygccw/website/pc/index/controller/IndexController.java
@@ -1,16 +1,20 @@
 package com.ygccw.website.pc.index.controller;
 
+import com.ygccw.website.database.FindResultToSale;
 import com.ygccw.website.pc.index.service.IndexWebService;
+import com.ygccw.website.pc.info.service.InfoWebService;
+import com.ygccw.website.utils.PageUtils;
 import com.ygccw.wechat.common.info.enums.InfoVideoType;
 import com.ygccw.wechat.common.recommend.enums.RecommendLocal;
 import com.ygccw.wechat.common.recommend.enums.RecommendType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author soldier
@@ -19,6 +23,8 @@ import javax.inject.Inject;
 public class IndexController {
     @Inject
     private IndexWebService indexWebService;
+    @Inject
+    private InfoWebService infoWebService;
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index(final ModelMap model) {
@@ -42,12 +48,30 @@ public class IndexController {
         return "/index.html";
     }
 
-    @RequestMapping(value = "/search.html", method = RequestMethod.GET)
-    public String search(final ModelMap model, @RequestParam(required = false) String keywords, @RequestParam(required = false) String type, @RequestParam(defaultValue = "1") Integer currentPage) {
+    @RequestMapping(value = "/search_{keywords}_{type}_{currentPage}.html", method = RequestMethod.GET)
+    public String search(HttpServletRequest request, final ModelMap model, @PathVariable String keywords, @PathVariable String type, @PathVariable Integer currentPage) {
         int fetchSize = 16;
-        if (type == null) {
-
+        if ("all".equals(type)) {
+            fetchSize = 4;
+            model.put("infoList", new FindResultToSale(indexWebService.searchInfo(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchInfoSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
+            model.put("matchZoneList", new FindResultToSale(indexWebService.searchMatchZone(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchMatchZoneSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
+            model.put("anchorZoneList", new FindResultToSale(indexWebService.searchAnchorZone(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchAnchorZoneSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
+            model.put("videoList", new FindResultToSale(indexWebService.searchVideo(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchVideoSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
+            model.put("pictureList", new FindResultToSale(indexWebService.searchPicture(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchPictureSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
+        } else if ("news".equals(type)) {
+            model.put("newsList", new FindResultToSale(indexWebService.searchInfo(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchInfoSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
+        } else if ("video".equals(type)) {
+            model.put("videoList", new FindResultToSale(indexWebService.searchVideo(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchVideoSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
+        } else if ("match".equals(type)) {
+            model.put("matchZoneList", new FindResultToSale(indexWebService.searchMatchZone(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchMatchZoneSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
+        } else if ("anchor".equals(type)) {
+            model.put("anchorZoneList", new FindResultToSale(indexWebService.searchAnchorZone(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchAnchorZoneSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
+        } else if ("picture".equals(type)) {
+            model.put("pictureList", new FindResultToSale(indexWebService.searchPicture(keywords, PageUtils.getStartRecord(currentPage, fetchSize), fetchSize), indexWebService.searchPictureSize(keywords), currentPage, fetchSize, PageUtils.getPageUrl(request)));
         }
+        model.put("newsTopList", infoWebService.newsListTop(0, 10));
+        model.put("anchorTopList", infoWebService.anchorListTop(0, 6));
+        model.put("videoTopList", infoWebService.videoListTop(0, 4));
         return "/search.html";
     }
 }
